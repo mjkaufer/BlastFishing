9c9b87e18adec6636894fce68263ffb99aea7425
mjkaufer <mjkaufer@gmail.com>
2014-12-30 15:29:26 -0500

    Basic shell
    Can launch items

---
+++ src/org/kaufer/matthew/BlastFishing/BlastFishing.java
@@ -0,0 +1,62 @@
+package org.kaufer.matthew.BlastFishing;
+
+
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+public class BlastFishing extends JavaPlugin{
+	public final Logger logger = Logger.getLogger("Minecraft");
+	private BlastFishingListener listener = null;
+	private PluginDescriptionFile p;
+	@Override
+	public void onEnable(){
+		p = this.getDescription();
+		this.logger.info(p.getName() + " V" + p.getVersion() + " has been enabled.");
+        PluginManager pm = this.getServer().getPluginManager();
+        listener = new BlastFishingListener(this);
+        pm.registerEvents((Listener)listener , this);
+	}
+	
+	@Override
+	public void onDisable(){
+//		PluginDescriptionFile p = this.getDescription();
+		this.logger.info(p.getName() + " V" + p.getVersion() + " has been disabled.");   
+		getServer().clearRecipes();
+	}
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+    {
+            Player player = (Player)sender;
+            if(cmd.getName().equalsIgnoreCase("blastfishing"))
+            {
+            	if(args.length == 0){
+                    player.sendMessage(ChatColor.AQUA + p.getName() + ChatColor.GREEN + " V" + p.getVersion() + ChatColor.AQUA + " , by " + ChatColor.RED + "mjkaufer");
+            	}
+            	else{
+            		Chunk[] loaded = player.getWorld().getLoadedChunks();
+            		for(Chunk c : loaded){
+            			Entity[] entities = c.getEntities();
+            			for(Entity e : entities){
+            				if(e instanceof Item){//item drop, so we'll screw with it
+            					e.setVelocity(new Vector(Math.random() - 0.5, Math.random() + 1, Math.random() - 0.5).multiply(0.75));
+            				}
+            			}
+            		}
+            	}
+            }
+            return false;
+            
+    }
+	
+}
---
+++ src/org/kaufer/matthew/BlastFishing/BlastFishingListener.java
@@ -0,0 +1,16 @@
+package org.kaufer.matthew.BlastFishing;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class BlastFishingListener implements Listener{
+	private BlastFishing plugin;
+	
+	public BlastFishingListener(BlastFishing instance){
+		plugin = instance;
+	}
+	
+	
+//    @EventHandler
+
+}
